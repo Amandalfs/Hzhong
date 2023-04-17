04df53f3f505f721cd52bf0aecfc65c0b27b2ae6
Amandalfs <caduc57@gmail.com>
2023-04-17 13:37:41 -0300

    feat
    a mostra de saldo no deposito independente

--- assets/js/depositar.js
+++ assets/js/depositar.js
@@ -1,11 +1,21 @@
 window.addEventListener("load", monstrarDados());
 
-function monstrarDados(){
+async function monstrarDados(){
+    async function apiRender(username){
+        const dados = await fetch(`https://api-bancohzhong.cyclic.app/users/userAconnt?username=${username}`)
+            .then(result => result.json())
+                .then(data => {return data.dados})
+                    .catch((e)=> {return false});
+        return dados;
+    }
+
+    
     const dados = JSON.parse(localStorage.getItem('bank:client'));
     const name_perfil = document.querySelector('.name_perfil');
     const saldo_value = document.querySelector('.saldo_value');
+    const { saldo } = await apiRender(dados.username)
     name_perfil.innerHTML = `${dados.username}`
-    saldo_value.innerHTML = `R$ ${dados.Saldo},00`
+    saldo_value.innerHTML = `R$${saldo},00`
 }
 
 async function depositar(){
@@ -36,10 +46,9 @@ async function depositar(){
         error.innerHTML = ``
         const popUp = document.querySelector('.pop-up');
         const saque_span = document.querySelector('.deposito_span');
-        saque_span.innerHTML = `${depositar_value}`
+        saque_span.innerHTML = `${Number(depositar_value).toFixed(2).replace('.',',')}`
         popUp.style =  'opacity: 1';
         popUp.style.transform = 'scale(1.1)';
-        console.log(result)
     }
 }
 
