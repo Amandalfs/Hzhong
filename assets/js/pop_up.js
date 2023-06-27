59eaee8a945c7aa8e1c36ea964a82e4655e4c52a
Amandalfs <amandalfs57@gmail.com>
2023-06-27 15:22:35 -0300

    limpado repositorio:

--- assets/js/pop_up.js
+++
@@ -1,7 +0,0 @@
-const btnClose = document.querySelector('.btn_close');
-btnClose.addEventListener('click',()=>{
-    const popUp = document.querySelector('.pop-up');
-    popUp.style =  'opacity: 0';
-    popUp.style.transform = 'scale(0.8)';
-    window.location.reload()
-})
