359cf886b64667a184c0754d0b67db4dc1f73b32
Amandalfs <amandalfs57@gmail.com>
2023-06-27 15:16:55 -0300

    test

--- src/app/components/ButtonRadius/index.tsx
+++ src/app/components/ButtonRadius/index.tsx
@@ -6,6 +6,7 @@ export interface IButtonRadius {
 export function ButtonRadius({title}:IButtonRadius){
     return (<button className="flex flex-col items-center text-green-50 rounded-3xl h-12 bg-purple-700 text-center justify-center w-44"
       >
+        
         {title}
     </button>)
 }
