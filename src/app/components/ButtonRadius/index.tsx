eedbc08b05b811d27a6686736f676a92df11ea17
Amandalfs <amandalfs57@gmail.com>
2023-06-28 11:06:39 -0300

    refactor: movido o style do buttonRadius para o styled componets

--- src/app/components/ButtonRadius/index.tsx
+++ src/app/components/ButtonRadius/index.tsx
@@ -1,12 +1,13 @@
+'use client';
+
+import { ButtonRadiusContainer } from "./style";
+
 export interface IButtonRadius {
     title: string,
-    btnExec: Function,
 }
 
 export function ButtonRadius({title}:IButtonRadius){
-    return (<button className="flex flex-col items-center text-green-50 rounded-3xl h-12 bg-purple-700 text-center justify-center w-44"
-      >
-        
+    return (<ButtonRadiusContainer>
         {title}
-    </button>)
+    </ButtonRadiusContainer>)
 }
---
+++ src/app/components/ButtonRadius/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ButtonRadiusContainer = styled.button`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+    color: #eaeaea;
+    background-color: #6D45C0;
+    height: 3rem;
+    width: 11rem;
+    border-radius: 1.5rem;
+`
