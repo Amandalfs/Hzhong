3313730902c811c092d8d9c265fb95a7963af039
Amandalfs <amandalfs57@gmail.com>
2023-07-17 11:32:09 -0300

    feat: criar o link do marketPlace

---
+++ src/app/components/MarketPlaceMenu/CardMarketPlace/index.tsx
@@ -0,0 +1,9 @@
+import { CardMenuContainer, LinkMenu } from "../../Menu/Card/style";
+
+export  function CardMarketPlace(){
+    return (<CardMenuContainer>
+        <LinkMenu href={"/breve"}>
+            Em breve
+        </LinkMenu>
+    </CardMenuContainer>)
+}
