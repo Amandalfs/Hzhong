d5be575d0f4ad81e8c72e8749d3636b74fe23ee5
Amandalfs <amandalfs57@gmail.com>
2023-07-20 16:44:51 -0300

    style: ajustar o header

--- src/app/components/HeaderHome/index.tsx
+++ src/app/components/HeaderHome/index.tsx
@@ -7,16 +7,16 @@ import logoWhite from "../../../assets/logoWhite.png"
 
 export function HeaderHome(){
     return (<header className="flex flex-col">
-        <nav className="flex flex-row m-auto mt-2 justify-between w-[85rem]">
-            <div className="flex flex-row gap-4">
+        <nav className="flex flex-row m-auto mt-2  lg:gap-0 justify-between lg:justify-between w-[90%]">
+            <div className="flex flex-row  gap-4 lg:gap-8">
                 <MarketPlaceMenu />
                 <ProfileMenu />
             </div>
             
             <SignInSignUpMenu />
         </nav>
-        <div className="flex flex-row w-[85rem] h-1 m-auto mt-2 mb-2 border-black-100 border-t-2 " ></div>
-        <div className="flex flex-row w-[85rem] m-auto justify-between items-center">
+        <div className="flex flex-row w-[90%] h-1 m-auto mt-2 mb-2 border-black-100 border-t-2 " ></div>
+        <div className="flex flex-row w-[90%] m-auto justify-between items-center">
             <Image src={logoWhite} alt=""/>
             <p>Menu de saque</p>
         </div>
