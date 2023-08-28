84550ee6e955a634c1287f0c3363f982f7d80fab
Amandalfs <amandalfs57@gmail.com>
2023-08-28 16:08:17 -0300

    refactor: create validate authenticated signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,29 @@
+"use client";
 import { FormStepperRegister } from "../components/FormStepperRegister";
 import { HeaderHome } from "../components/HeaderHome";
 import { UserRegisterContextProvider } from "../contexts/userRegisterContext";
 import Image  from "next/image";
 
 import background from "@/assets/backgroundSingUp.png";
+import { useContext, useEffect } from "react";
+import { CookiesContext } from "../contexts/cookiesContext";
+import { useCookies } from "../hooks/cookiesHook";
+import { useRouter } from "next/navigation";
 
 export default function SignUp(){
+	const router = useRouter();
+	const { cookies } = useContext(CookiesContext);
+	const isAuthenticated = cookies.token;
+	const { getCookie } = useCookies();
+
+	useEffect(()=>{
+    	getCookie("token");
+    	if(isAuthenticated){
+    		router.push("/dashboard");
+    	}
+	},[]);
+
+
 	return (<UserRegisterContextProvider>
 		<main>
 			<HeaderHome />
