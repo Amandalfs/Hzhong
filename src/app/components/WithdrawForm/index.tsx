708c823512fc281ebbba7d05a2e88dc82da2fc63
Amandalfs <amandalfs57@gmail.com>
2023-08-29 18:09:17 -0300

    feat: Implementing the functionality to withdraw an amount from the account.

--- src/app/components/WithdrawForm/index.tsx
+++ src/app/components/WithdrawForm/index.tsx
@@ -3,18 +3,64 @@ import { Input } from "@chakra-ui/react";
 import Image from "next/image";
 import { ButtonRadius } from "../ButtonRadius";
 import stroke from "@/assets/strokes.svg";
+import { useContext } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import * as zod from "zod";
+import { CookiesContext } from "@/app/contexts/cookiesContext";
+import { useForm } from "react-hook-form";
+import { apiHzhong } from "@/app/services/api";
+import { ToastContainer, toast } from "react-toastify";
 
 export function WithdrawForm(){
-	return (<form className="flex flex-col gap-8">
-		<div className="flex flex-col gap-2">
-			<div className="flex gap-1">
-				<Image src={stroke} alt="" />
-				<h1 className="text-purple-700 text-3xl font-sans font-bold">Saldo da conta</h1>
-			</div>
-			<h2 className="text-[#84DA97] text-base font-sans">Credit Balance</h2>
-			<h1 className="text-blue-950 text-2xl font-sans font-bold">$4,500,093.00</h1>
-		</div>
-		<Input placeholder='Valor de saque' size='sm' type="number" isRequired={true} width="xs" />
-		<ButtonRadius title="Sacar" typeButton="submit" variantSize="large" />
-	</form>);
+	const { cookies } = useContext(CookiesContext);
+	const dataWithdradValidSchema = zod.object({
+		withdraw: zod.coerce.number()
+	});
+    
+    type IDataWithdradValidSchema = zod.infer<typeof  dataWithdradValidSchema>
+
+    const { register, handleSubmit, reset, watch } = useForm<IDataWithdradValidSchema>({
+    	resolver: zodResolver(dataWithdradValidSchema),
+    	defaultValues: {
+    		withdraw: 1,
+    	}  
+    });
+
+    async function handleWithdraw({ withdraw }: IDataWithdradValidSchema) {
+    	try {
+    		const result = await apiHzhong.patch("/transactions/withdraw", {
+    			withdraw
+    		},{
+    			headers: {
+    				Authorization: `Bearer ${cookies.token}`
+    			}
+    		});
+    		toast.success(result.data.params.extratoNew.descricao);
+    		reset();
+    	} catch (error) {
+    		console.log(error);
+    		if(error.response.data){
+    			return toast.error(error.response.data.msg);
+    		}
+    		toast.error("Error depositing. Please try again later.");
+    	}
+
+    }
+
+    return (<form className="flex flex-col gap-8" onSubmit={handleSubmit(handleWithdraw)}>
+    	<ToastContainer />
+    	<div className="flex flex-col gap-2">
+    		<div className="flex gap-1">
+    			<Image src={stroke} alt="" />
+    			<h1 className="text-purple-700 text-3xl font-sans font-bold">Sacar</h1>
+    		</div>
+    		<h2 className="text-[#84DA97] text-base font-sans">Credit Balance</h2>
+    		<h1 className="text-blue-950 text-2xl font-sans font-bold">$4,500,093.00</h1>
+    	</div>
+    	<Input placeholder='Valor de saque' size='sm' type="number" isRequired={true} width="xs" 
+    		{ ...register("withdraw")}
+    	/>
+    	<ButtonRadius title="Sacar" typeButton="submit" variantSize="large" />
+    </form>);
 }
--- src/app/transactions/withdraw/page.tsx
+++ src/app/transactions/withdraw/page.tsx
@@ -1,10 +1,29 @@
-
+"use client";
+import { useContext, useEffect } from "react";
 import Image from "next/image";
 import { HeaderHome } from "@/app/components/HeaderHome/index";
 import { WithdrawForm } from "@/app/components/WithdrawForm";
 import background from "@/assets/transactions/TransctionsBackground.png";
 
+import { CookiesContext } from "@/contexts/cookiesContext";
+import { useCookies } from "@/app/hooks/cookiesHook";
+
+import { useRouter } from "next/navigation";
+
 export default function Withdraw(){
+
+	const router = useRouter();
+	const { cookies } = useContext(CookiesContext);
+	const isAuthenticated = cookies.token;
+	const { getCookie } = useCookies();
+
+	useEffect(()=>{
+		getCookie("token");
+		if(!isAuthenticated){
+			router.push("/signin");
+		}
+	},[]);
+
 	return (<div>
 		<HeaderHome />
 		<section className='flex justify-around items-center my-16'>
