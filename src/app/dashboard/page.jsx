3263053407fecaa63680c7eca77911f9b6232140
Amandalfs <amandalfs57@gmail.com>
2023-08-29 18:44:06 -0300

    feat: create check token dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,27 @@
-
+"use client";
 import { HeaderHome } from "./../components/HeaderHome";
 import { History } from "../components/History";
 import { StaticMoneyMonth } from "./../components/StaticMoneyMonth/index";
 
+import { useContext, useEffect } from "react";
+import { CookiesContext } from "../contexts/cookiesContext";
+import { useCookies } from "../hooks/cookiesHook";
+import { useRouter } from "next/navigation";
+
 export default function Dashboard(){
+
+	const router = useRouter();
+	const { cookies } = useContext(CookiesContext);
+	const isAuthenticated = cookies.token;
+	const { getCookie } = useCookies();
+
+	useEffect(()=>{
+    	getCookie("token");
+    	if(isAuthenticated){
+    		router.push("/dashboard");
+    	}
+	},[]);
+
 	return (<>
 		<HeaderHome />
 		<section className="my-40">
