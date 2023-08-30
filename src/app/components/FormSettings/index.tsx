67aeb124c3173d9645f72cb8fd276d7af5fcf11c
Amandalfs <amandalfs57@gmail.com>
2023-08-30 11:43:13 -0300

    feat: add contextUserInfos to formSettings and settings accont

--- src/app/components/FormSettings/index.tsx
+++ src/app/components/FormSettings/index.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { Input } from "@chakra-ui/react";
 import { Button } from "../Button";
+import { useContext } from "react";
+import { userInfosContext } from "@/app/contexts/userInfosContext";
 
 export function FormSettings(){
+	const { userInfos } = useContext(userInfosContext);
 	return (<form className="flex flex-col w-96 gap-12">
 		<div className="flex justify-between items-center">
 			<h2 className="text-blue-950 text-xl font-sans font-medium">Informações Basicas</h2>
@@ -12,13 +15,13 @@ export function FormSettings(){
 			</div>
 		</div>
 		<div className="flex flex-col gap-4 w-80">
-			<Input type="text" placeholder="Username" />
-			<Input type="text" placeholder="Nome" />
+			<Input type="text" placeholder="Username" defaultValue={userInfos.username} />
+			<Input type="text" placeholder="Nome" defaultValue={userInfos.name}/>
 			<Input type="text" placeholder="Senha" />
 			<Input type="text" placeholder="Nova senha" />
 		</div>
 		<div className="flex flex-col gap-4 w-80">
-			<Input type="text"  placeholder="Key Pix" />
+			<Input type="text"  placeholder="Key Pix" defaultValue={userInfos.keypix} />
 			<div className="flex flex-col gap-2">
 				<Button title="Criar" variantSize="small" variantButton="primary" />
 				<Button title="Excluir" variantSize="small" variantButton="segundary" />
