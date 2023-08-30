1704ef7e65cce025f267000d19349d6c894abd4d
Amandalfs <amandalfs57@gmail.com>
2023-08-30 12:17:06 -0300

    feat: add links StaticMoney

--- src/app/components/StaticMoneyMonth/index.tsx
+++ src/app/components/StaticMoneyMonth/index.tsx
@@ -5,6 +5,7 @@ import { ButtonRadius } from "../ButtonRadius";
 import { GraficStatics } from "./GraficStatics";
 import { useContext } from "react";
 import { userStaticHistoryContext } from "@/app/contexts/userStaticHistoryContext";
+import Link from "next/link";
 
 export function StaticMoneyMonth(){
 	const { userStatic } = useContext(userStaticHistoryContext);
@@ -20,18 +21,22 @@ export function StaticMoneyMonth(){
 		</div>
 		<div className="flex flex-col gap-8">
 			<div className="flex flex-col gap-2">
-				<h1 className="text-black-800 text-2xl font-sans font-bold">${userStatic.balance}</h1>
+				<h1 className="text-blue-950 text-2xl font-sans font-bold">${userStatic.balance}</h1>
 				<div className="border-t border-gray-300"></div>
 				<h3 className="text-green-400 text-base">Saldo</h3>
 			</div>
 			<div className="flex flex-col gap-2">
-				<h1 className="text-black-800 text-2xl font-sans font-bold">${userStatic.BalanceOutput}</h1>
+				<h1 className="text-blue-950 text-2xl font-sans font-bold">${userStatic.BalanceOutput}</h1>
 				<div className="border-t border-gray-300"></div>
 				<h3 className="text-red-600 text-base">Saldo de Retirada</h3>
 			</div>
 			<div className="flex justify-around">
-				<ButtonRadius title="Sacar" variantSize="large" />
-				<ButtonRadius title="Transferir" variantSize="large" />
+				<Link href={"/transactions/withdraw"}>
+					<ButtonRadius title="Sacar" variantSize="large" />
+				</Link>
+				<Link href={"/transactions/transferMoney"}>
+					<ButtonRadius title="Transferir" variantSize="large" />
+				</Link>
 			</div>
 		</div>
 	</div>);
