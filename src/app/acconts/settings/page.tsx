f0b6481d3897eb02366c3648a270452712c88503
Amandalfs <amandalfs57@gmail.com>
2023-09-13 13:25:27 -0300

    feat: integration edit user

--- src/app/acconts/settings/page.tsx
+++ src/app/acconts/settings/page.tsx
@@ -22,11 +22,12 @@ export default function Settings(){
 		}
 	},[]);
 
-	return (<div className="flex flex-col">
-		<HeaderHome />
-		<section className="flex flex-col justify-center items-center gap-16">
-			<h1 className="text-purple-700 text-lg font-sans font-bold">Configurações do perfil</h1>
-			<FormSettings />
-		</section>
-	</div>);
+	return (
+		<div className="flex flex-col">
+			<HeaderHome />
+			<section className="flex flex-col justify-center items-center gap-16">
+				<h1 className="text-purple-700 text-lg font-sans font-bold">Configurações do perfil</h1>
+				<FormSettings />
+			</section>
+		</div>);
 }
